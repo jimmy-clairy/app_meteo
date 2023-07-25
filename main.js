951bb0c7c229067bdcad81413a67b5bff31b1833
Jimmy Clairy <jimcl@hotmail.fr>
2023-07-25 19:27:58 +0200

    End App meteo

---
+++ main.js
@@ -0,0 +1,52 @@
+async function fetchData() {
+    try {
+        // Address doc https://api-docs.iqair.com/?version=latest
+        const resJSON = await fetch('http://api.airvisual.com/v2/nearest_city?key=f65412e8-a270-4b50-ab86-81ba079581aa');
+        if (!resJSON.ok) {
+            throw new Error('Connection serveur impossible');
+        }
+        const resJS = await resJSON.json();
+
+        const dataMeteo = {
+            country: resJS.data.country,
+            city: resJS.data.city,
+            temp: resJS.data.current.weather.tp,
+            icon: resJS.data.current.weather.ic
+        };
+        createElements(dataMeteo);
+
+    } catch (error) {
+        console.error("===Error=== " + error);
+        const errorInfo = document.querySelector('.error-info');
+        errorInfo.style.color = 'red';
+        errorInfo.style.display = "flex";
+        errorInfo.innerText = error.message;
+    } finally {
+        const spinner = document.querySelector('.lds-spinner');
+        spinner.classList.add('hidden');
+        setTimeout(() => spinner.remove(), 800);
+    }
+}
+
+/**
+ * Creates DOM elements to display the weather data.
+ * @function createElements
+ * @param {Object} dataMeteo - The weather data object.
+ * @param {string} dataMeteo.country - The country name.
+ * @param {string} dataMeteo.city - The city name.
+ * @param {number} dataMeteo.temp - The temperature value.
+ * @param {string} dataMeteo.icon - The icon representing weather conditions.
+ */
+function createElements(dataMeteo) {
+    const city = document.querySelector('.city');
+    const country = document.querySelector('.country');
+    const temp = document.querySelector('.temp');
+    const icon = document.querySelector('.icon');
+
+    city.textContent = dataMeteo.city;
+    country.textContent = dataMeteo.country;
+    temp.textContent = `${dataMeteo.temp}°`;
+    icon.src = `ressources/jour/${dataMeteo.icon}.svg`;
+}
+
+fetchData();
